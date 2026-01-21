276f226edab1a54546cc1f9e8b1982a4adaa49b1
saphal4859 <saisaphal123@gmail.com>
2026-01-21 22:22:11 +0530

    Enabled cors

--- src/main/java/com/prep/interviewprep/config/SecurityConfig.java
+++ src/main/java/com/prep/interviewprep/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
 
     config.setAllowedOrigins(List.of(
         "http://localhost:5173",
-        "http://localhost:8080"
+        "http://localhost:8080",
+        "https://interview-questions-prep-ui.vercel.app"
     ));
     config.setAllowedMethods(List.of(
         "GET", "POST", "PUT", "DELETE", "OPTIONS"
