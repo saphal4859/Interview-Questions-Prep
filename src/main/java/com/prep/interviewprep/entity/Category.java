cacfab8e171ec472c622c2dc5ece2ddc4a026edf
saphal4859 <saisaphal123@gmail.com>
2026-04-08 23:55:59 +0530

    Added categories

--- src/main/java/com/prep/interviewprep/entity/Category.java
+++ src/main/java/com/prep/interviewprep/entity/Category.java
@@ -1,7 +1,13 @@
 package com.prep.interviewprep.entity;
 
+import org.hibernate.annotations.processing.SQL;
+
 public enum Category {
   JAVA,
+  SPRING_CORE,
+  SPRING_SECURITY,
+  SPRING_DATA_JPA,
+  SPRING_AOP,
   SPRING_BOOT,
   MICROSERVICES,
   EVENT_DRIVEN_ARCHITECTURE,
